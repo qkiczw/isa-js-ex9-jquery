277161a9131a93b483c89c2b8d832b33c11ae3ec
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-01-18 20:24:13 +0100

    Add code to add dynamic form and add user to array

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function() {
+// $(function() {
 
     const users = [
         {firstName: 'Tomasz', lastName: 'Doe', age: 23, city: 'London', id: 1},
@@ -54,12 +54,60 @@ $(function() {
 
     const $Highlight = function () {
         $(this).toggleClass('bg-success');
-    }
+    };
 
     $tableRow.on({
         mouseenter: $Highlight,
         mouseleave: $Highlight
-    });
+        });
 
 
- });
+    const $userForm = $(`
+        <form class="col-sm-12 col-md-5 pl-5 pr-5 pt-2 pb-3">
+        <div class="form-group">
+            <label for="form-id">id</label>
+            <input type="text" class="form-control" id="form-id" placeholder=" Wpisz Id">
+        </div>
+        <div class="form-group">
+            <label for="form-first-name">Imię</label>
+            <input type="text" class="form-control" id="form-first-name" placeholder="Wpisz imię">
+        </div>
+        <div class="form-group">
+            <label for="form-last-name">Nazwisko</label>
+            <input type="text" class="form-control" id="form-last-name" placeholder="Wpisz nazwisko">
+        </div>
+        <div class="form-group">
+            <label for="form-age">Wiek</label>
+            <input type="text" class="form-control" id="form-age" placeholder="Wpisz wiek">
+        </div>
+        <div class="form-group">
+            <label for="form-city">Miasto</label>
+            <input type="text" class="form-control" id="form-city" placeholder="Wpisz miasto">
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="check-age">
+            <label class="form-check-label" for="check-age">Mam 18 lat.</label>
+        </div>
+        <button id="add-user-btn" class="btn btn-primary">Dodaj</button>
+        </form>`);
+
+    $userForm.prependTo('body');
+    const $formId= $('#form-id');
+    const $formFirstName = $('#form-first-name');
+    const $formLastName = $('#form-last-name');
+    const $formAge = $('#form-age');
+    const $formCity = $('#form-city');
+    const $addUserBtn = $('#add-user-btn');
+
+    const $newUser = {};
+
+
+    function getNewUser() {
+        event.preventDefault();
+        users.push({firstName: $formFirstName.val(), lastName: $formLastName.val(), age: $formAge.val(), city: $formCity.val(), id: users.length + 1})
+    }
+
+
+    $addUserBtn.on('click', getNewUser);
+
+ // });
